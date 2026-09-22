2920ad176a588204cc3099921d22291ee9739a55
Juan-Galiano <galianojuan.18@gmail.com>
2026-09-22 20:34:28 -0300

    feat: agregar flujo de estados a solicitudes
    Agrega los estados Anulada y Cerrada, junto con reglas para asignar, reasignar, iniciar, resolver, anular y cerrar solicitudes.

--- src/Gestiones.Domain/Entities/Solicitud.cs
+++ src/Gestiones.Domain/Entities/Solicitud.cs
@@ -28,12 +28,96 @@ namespace Gestiones.Domain.Entities
             Estado = EstadoSolicitud.Creada;
             OperarioAsignado = null;
         }
+
+
         public void AsignarOperario(Empleado empleado)
         {
+            if (Estado == EstadoSolicitud.Resuelta ||
+                Estado == EstadoSolicitud.Cerrada || Estado == EstadoSolicitud.Anulada)
+            {
+                throw new InvalidOperationException(
+                   "No se puede asignar un operario a una solicitud resuelta, anulada o cerrada."
+                );
+            }
+
+            if (OperarioAsignado != null)
+            {
+                throw new InvalidOperationException("La solicitud ya tiene un operario asignado.");
+            }
+
             OperarioAsignado = empleado;
             Estado = EstadoSolicitud.Asignada;
+        }
 
-           
+        public void ReasignarOperario(Empleado empleado)
+        {
+            if (Estado == EstadoSolicitud.Resuelta ||
+                Estado == EstadoSolicitud.Cerrada || Estado == EstadoSolicitud.Anulada)
+            {
+                throw new InvalidOperationException(
+                    "No se puede reasignar un operario a una solicitud resuelta, anulada o  cerrada."
+                );
+            }
+
+            if (OperarioAsignado == null)
+            {
+                throw new InvalidOperationException("No se puede reasignar un operario porque no esta asignada a ninguno.");
+            }
+
+            OperarioAsignado = empleado;
+        }
+
+
+        public void Iniciar()
+        {
+            if (Estado != EstadoSolicitud.Asignada)
+            {
+                throw new InvalidOperationException(
+                    "No se puede iniciar una solicitud que no esta Asignada."
+                );
+            }
+
+            Estado = EstadoSolicitud.EnProceso;
+        }
+
+
+        public void Resolver()
+        {
+            if (Estado != EstadoSolicitud.EnProceso)
+            {
+                throw new InvalidOperationException(
+                    "No se puede resolver una solicitud que no está en proceso."
+                );
+            }
+
+            Estado = EstadoSolicitud.Resuelta;
+        }
+
+        public void Anular()
+        {
+            if (Estado != EstadoSolicitud.Creada &&
+                Estado != EstadoSolicitud.Asignada)
+            {
+                throw new InvalidOperationException(
+                    "No se puede anular una solicitud que no está en estado creada o asignada."
+                );
+            }
+
+            Estado = EstadoSolicitud.Anulada;
+        }
+
+
+
+        public void Cerrar()
+        {
+            if (Estado != EstadoSolicitud.Resuelta)
+            {
+                throw new InvalidOperationException(
+                    "No se puede cerrar una solicitud que no está resuelta."
+                );
+            }
+
+            Estado = EstadoSolicitud.Cerrada;
         }
     }
 
--- src/Gestiones.Domain/Enums/EstadoSolicitud.cs
+++ src/Gestiones.Domain/Enums/EstadoSolicitud.cs
@@ -6,6 +6,7 @@
         Asignada,
         EnProceso,
         Resuelta,
+        Anulada,
         Cerrada
     }
 }
